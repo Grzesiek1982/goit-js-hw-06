0d71084d88bec005315f936327f2240d20641ec9
Grzesiek1982 <grzesieg1982@interia.pl>
2023-02-23 23:49:11 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+for (let i = 0; i <= ingredients.length; i += 1) {
+  const name = ingredients[i];
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("item");
+  itemEl.textContent = `${name}`;
+
+  const listEl = document.getElementById("ingredients");
+  listEl.append(itemEl);
+}
